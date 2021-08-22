9da953062e1f360d7ecc7edb8b1e739210f26528
MoneyMaker-l <946800781@qq.com>
2021-08-22 15:29:27 +0800

    dolike community

---
+++ src/main/java/com/lv/community/dto/LikeDTO.java
@@ -0,0 +1,13 @@
+package com.lv.community.dto;
+
+import lombok.Data;
+
+/**
+ * @author lvjiangtao
+ * @create 2021-08-22-14:22
+ */
+@Data
+public class LikeDTO {
+    private  Integer userId;
+    private  Integer questionId;
+}
---
+++ src/main/java/com/lv/community/dto/PublishQuestionDTO.java
@@ -0,0 +1,14 @@
+package com.lv.community.dto;
+
+import lombok.Data;
+
+/**
+ * @author lvjiangtao
+ * @create 2021-07-15-23:01
+ */
+@Data
+public class PublishQuestionDTO {
+    private String title;
+    private String description;
+    private String tag;
+}
